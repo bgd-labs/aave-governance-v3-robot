56587359a6b2d58ef9ea214d7958e42a30a0f328
Harsh Pandey <harshsatishpandey@gmail.com>
2024-03-13 22:19:21 +0530

    feat: update robot operator

--- src/contracts/AaveCLRobotOperator.sol
+++ src/contracts/AaveCLRobotOperator.sol
@@ -1,12 +1,14 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.18;
 
 import {IAaveCLRobotOperator} from '../interfaces/IAaveCLRobotOperator.sol';
+import {IInitializableRobotOperator} from '../interfaces/IInitializableRobotOperator.sol';
 import {IKeeperRegistrar} from '../interfaces/IKeeperRegistrar.sol';
 import {IKeeperRegistry} from '../interfaces/IKeeperRegistry.sol';
 import {IERC20} from 'solidity-utils/contracts/oz-common/interfaces/IERC20.sol';
 import {SafeERC20} from 'solidity-utils/contracts/oz-common/SafeERC20.sol';
 import {OwnableWithGuardian} from 'solidity-utils/contracts/access-control/OwnableWithGuardian.sol';
+import {Initializable} from 'solidity-utils/contracts/transparent-proxy/Initializable.sol';
 
 /**
  * @title AaveCLRobotOperator
@@ -15,39 +17,30 @@ import {OwnableWithGuardian} from 'solidity-utils/contracts/access-control/Ownab
  *      The contract can register keepers, cancel it, pause it, withdraw excess link,
  *      refill the keeper, configure the keeper.
  */
-contract AaveCLRobotOperator is OwnableWithGuardian, IAaveCLRobotOperator {
+contract AaveCLRobotOperator is OwnableWithGuardian, Initializable, IAaveCLRobotOperator {
   using SafeERC20 for IERC20;
 
-  /// @inheritdoc IAaveCLRobotOperator
-  address public immutable LINK_TOKEN;
+  uint256[] internal _keepersList;
+  mapping(uint256 id => KeeperInfo) internal _keepersInfo;
 
-  /// @inheritdoc IAaveCLRobotOperator
-  address public immutable KEEPER_REGISTRY;
-
-  /// @inheritdoc IAaveCLRobotOperator
-  address public immutable KEEPER_REGISTRAR;
-
+  address internal _keeperRegistry;
+  address internal _keeperRegistrar;
+  address internal _linkToken;
   address internal _linkWithdrawAddress;
 
-  mapping(uint256 id => KeeperInfo) internal _keepers;
-
-  /**
-   * @param keeperRegistry address of the chainlink registry.
-   * @param keeperRegistrar address of the chainlink registrar.
-   * @param linkWithdrawAddress withdrawal address to send the exccess link after cancelling the keeper.
-   * @param operatorOwner address to set as the owner of the operator contract.
-   */
-  constructor(
+  /// @inheritdoc IInitializableRobotOperator
+  function initialize(
     address keeperRegistry,
     address keeperRegistrar,
     address linkWithdrawAddress,
     address operatorOwner
-  ) {
-    KEEPER_REGISTRY = keeperRegistry;
-    KEEPER_REGISTRAR = keeperRegistrar;
-    LINK_TOKEN = IKeeperRegistry(KEEPER_REGISTRY).getLinkAddress();
+  ) external initializer {
+    _keeperRegistry = keeperRegistry;
+    _keeperRegistrar = keeperRegistrar;
     _linkWithdrawAddress = linkWithdrawAddress;
     _transferOwnership(operatorOwner);
+    _linkToken = IKeeperRegistry(_keeperRegistry).getLinkAddress();
+    emit Initialized(keeperRegistry, keeperRegistrar, linkWithdrawAddress, operatorOwner);
   }
 
   /// @notice In order to fund the keeper we need to approve the Link token amount to this contract
@@ -60,7 +53,7 @@ contract AaveCLRobotOperator is OwnableWithGuardian, IAaveCLRobotOperator {
     uint8 triggerType,
     bytes memory triggerConfig
   ) external onlyOwner returns (uint256) {
-    IERC20(LINK_TOKEN).safeTransferFrom(msg.sender, address(this), amountToFund);
+    IERC20(_linkToken).safeTransferFrom(msg.sender, address(this), amountToFund);
 
     IKeeperRegistrar.RegistrationParams memory params = IKeeperRegistrar.RegistrationParams({
       name: name, // name of the keeper to register
@@ -75,12 +68,14 @@ contract AaveCLRobotOperator is OwnableWithGuardian, IAaveCLRobotOperator {
       amount: amountToFund // amount of link to fund the keeper with
     });
 
-    IERC20(LINK_TOKEN).forceApprove(KEEPER_REGISTRAR, amountToFund);
-    uint256 id = IKeeperRegistrar(KEEPER_REGISTRAR).registerUpkeep(params);
+    IERC20(_linkToken).forceApprove(_keeperRegistrar, amountToFund);
+    uint256 id = IKeeperRegistrar(_keeperRegistrar).registerUpkeep(params);
 
     if (id != 0) {
-      _keepers[id].upkeep = upkeepContract;
-      _keepers[id].name = name;
+      _keepersInfo[id].upkeep = upkeepContract;
+      _keepersInfo[id].name = name;
+      _keepersList.push(id);
+
       emit KeeperRegistered(id, upkeepContract, amountToFund);
 
       return id;
@@ -91,46 +86,65 @@ contract AaveCLRobotOperator is OwnableWithGuardian, IAaveCLRobotOperator {
 
   /// @inheritdoc IAaveCLRobotOperator
   function cancel(uint256 id) external onlyOwner {
-    IKeeperRegistry(KEEPER_REGISTRY).cancelUpkeep(id);
-    emit KeeperCancelled(id, _keepers[id].upkeep);
+    IKeeperRegistry(_keeperRegistry).cancelUpkeep(id);
+
+    // remove the keeper from the keepers list
+    for (uint256 index = 0; index < _keepersList.length; index++) {
+      if (_keepersList[index] == id) {
+        _keepersList[index] = _keepersList[_keepersList.length - 1];
+        _keepersList.pop();
+        break;
+      }
+    }
+
+    emit KeeperCancelled(id, _keepersInfo[id].upkeep);
   }
 
   /// @inheritdoc IAaveCLRobotOperator
   function withdrawLink(uint256 id) external {
-    IKeeperRegistry(KEEPER_REGISTRY).withdrawFunds(id, _linkWithdrawAddress);
-    emit LinkWithdrawn(id, _keepers[id].upkeep, _linkWithdrawAddress);
+    IKeeperRegistry(_keeperRegistry).withdrawFunds(id, _linkWithdrawAddress);
+    emit LinkWithdrawn(id, _keepersInfo[id].upkeep, _linkWithdrawAddress);
   }
 
   /// @notice In order to refill the keeper we need to approve the Link token amount to this contract
   /// @inheritdoc IAaveCLRobotOperator
   function refillKeeper(uint256 id, uint96 amount) external {
-    IERC20(LINK_TOKEN).safeTransferFrom(msg.sender, address(this), amount);
-    IERC20(LINK_TOKEN).forceApprove(KEEPER_REGISTRY, amount);
-    IKeeperRegistry(KEEPER_REGISTRY).addFunds(id, amount);
+    IERC20(_linkToken).safeTransferFrom(msg.sender, address(this), amount);
+    IERC20(_linkToken).forceApprove(_keeperRegistry, amount);
+    IKeeperRegistry(_keeperRegistry).addFunds(id, amount);
     emit KeeperRefilled(id, msg.sender, amount);
   }
 
   /// @inheritdoc IAaveCLRobotOperator
   function pause(uint256 id) external onlyOwnerOrGuardian {
-    IKeeperRegistry(KEEPER_REGISTRY).pauseUpkeep(id);
+    IKeeperRegistry(_keeperRegistry).pauseUpkeep(id);
     emit KeeperPaused(id);
   }
 
   /// @inheritdoc IAaveCLRobotOperator
   function unpause(uint256 id) external onlyOwnerOrGuardian {
-    IKeeperRegistry(KEEPER_REGISTRY).unpauseUpkeep(id);
+    IKeeperRegistry(_keeperRegistry).unpauseUpkeep(id);
     emit KeeperUnpaused(id);
   }
 
   /// @inheritdoc IAaveCLRobotOperator
+  function migrate(address newRegistry, address newRegistrar) external onlyOwner {
+    IKeeperRegistry(_keeperRegistry).migrateUpkeeps(_keepersList, newRegistry);
+
+    setRegistry(newRegistry);
+    setRegistrar(newRegistrar);
+    emit KeepersMigrated(_keepersList, newRegistry, newRegistrar);
+  }
+
+  /// @inheritdoc IAaveCLRobotOperator
   function setGasLimit(uint256 id, uint32 gasLimit) external onlyOwnerOrGuardian {
-    IKeeperRegistry(KEEPER_REGISTRY).setUpkeepGasLimit(id, gasLimit);
-    emit GasLimitSet(id, _keepers[id].upkeep, gasLimit);
+    IKeeperRegistry(_keeperRegistry).setUpkeepGasLimit(id, gasLimit);
+    emit GasLimitSet(id, _keepersInfo[id].upkeep, gasLimit);
   }
 
   /// @inheritdoc IAaveCLRobotOperator
   function setTriggerConfig(uint256 id, bytes calldata triggerConfig) external onlyOwnerOrGuardian {
-    IKeeperRegistry(KEEPER_REGISTRY).setUpkeepTriggerConfig(id, triggerConfig);
+    IKeeperRegistry(_keeperRegistry).setUpkeepTriggerConfig(id, triggerConfig);
     emit TriggerConfigSet(id);
   }
 
@@ -141,17 +155,49 @@ contract AaveCLRobotOperator is OwnableWithGuardian, IAaveCLRobotOperator {
   }
 
   /// @inheritdoc IAaveCLRobotOperator
+  function setRegistry(address newRegistry) public onlyOwner {
+    _keeperRegistry = newRegistry;
+    emit KeeperRegistrySet(newRegistry);
+  }
+
+  /// @inheritdoc IAaveCLRobotOperator
+  function setRegistrar(address newRegistrar) public onlyOwner {
+    _keeperRegistrar = newRegistrar;
+    emit KeeperRegistrarSet(newRegistrar);
+  }
+
+  /// @inheritdoc IAaveCLRobotOperator
   function getWithdrawAddress() external view returns (address) {
     return _linkWithdrawAddress;
   }
 
   /// @inheritdoc IAaveCLRobotOperator
   function getKeeperInfo(uint256 id) external view returns (KeeperInfo memory) {
-    return _keepers[id];
+    return _keepersInfo[id];
   }
 
   /// @inheritdoc IAaveCLRobotOperator
   function isPaused(uint256 id) external view returns (bool) {
-    return IKeeperRegistry(KEEPER_REGISTRY).getUpkeep(id).paused;
+    return IKeeperRegistry(_keeperRegistry).getUpkeep(id).paused;
+  }
+
+  /// @inheritdoc IAaveCLRobotOperator
+  function getRegistry() public view returns (address) {
+    return _keeperRegistry;
+  }
+
+  /// @inheritdoc IAaveCLRobotOperator
+  function getRegistrar() public view returns (address) {
+    return _keeperRegistrar;
+  }
+
+  /// @inheritdoc IAaveCLRobotOperator
+  function getKeepersList() public view returns (uint256[] memory) {
+    return _keepersList;
+  }
+
+  /// @inheritdoc IAaveCLRobotOperator
+  function getLinkToken() external view returns (address) {
+    return _linkToken;
   }
 }
---
+++ src/interfaces/IInitializableRobotOperator.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/**
+ * @title IInitializableRobotOperator
+ * @author BGD Labs
+ * @notice Interface for the initialize function on AaveCLRobotOperator
+ **/
+interface IInitializableRobotOperator {
+  /**
+   * @dev Emitted when a AaveCLRobotOperator is initialized
+   * @param keeperRegistry address of the chainlink registry.
+   * @param keeperRegistrar address of the chainlink registrar.
+   * @param linkWithdrawAddress withdrawal address of the operator contract.
+   * @param operatorOwner owner of the operator contract.
+   **/
+  event Initialized(
+    address keeperRegistry,
+    address keeperRegistrar,
+    address linkWithdrawAddress,
+    address operatorOwner
+  );
+
+  /**
+   * @dev Initializes the AaveCLRobotOperator
+   * @param keeperRegistry address of the chainlink registry.
+   * @param keeperRegistrar address of the chainlink registrar.
+   * @param linkWithdrawAddress withdrawal address to send the exccess link after cancelling the keeper.
+   * @param operatorOwner address to set as the owner of the operator contract.
+   */
+  function initialize(
+    address keeperRegistry,
+    address keeperRegistrar,
+    address linkWithdrawAddress,
+    address operatorOwner
+  ) external;
+}
